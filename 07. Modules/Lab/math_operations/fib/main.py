9b6224f552688edeb91169d7ba453092a5102b48
piziamko <stoyanovasibina@gmail.com>
2021-06-11 23:10:22 +0300

    Create main.py

---
+++ 07. Modules/Lab/math_operations/fib/main.py
@@ -0,0 +1,20 @@
+from advanced_modules.fib.helper import generate_sequence, find_number
+
+
+command = input()
+
+while not command == "Stop":
+    data = command.split()
+    if data[0] == "Create":
+        fib_nums = generate_sequence(int(data[-1]))
+        print(fib_nums)
+    else:
+        result = find_number(int(data[-1]), fib_nums)
+        print(f"The number - {int(data[-1])} is "
+              f"at index {result}" if isinstance(result, int)
+              else result)
+    command = input()
+
+    © 2021 GitHub, Inc.
+    Terms
+    Privacy
