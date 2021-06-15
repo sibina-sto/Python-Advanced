9cb983ff7593b06170bbf9ac92dfcce3b8c9496a
piziamko <stoyanovasibina@gmail.com>
2021-06-15 20:42:25 +0300

    Update utils.py

--- 09. Workshop/Tic Tac Toe/utils.py
+++ 09. Workshop/Tic Tac Toe/utils.py
@@ -1,11 +1,11 @@
 def decode_position(position, board_size):
     row = (position - 1) // board_size
-    col = (position - 1) % board_size
-    return row, col
+    column = (position - 1) % board_size
+    return (row, column)
 
 
-def all_single_value(ll, mark):
-    for cell in ll:
-        if not cell == mark:
+def all_single_value(ll, value):
+    for v in ll:
+        if v != value:
             return False
     return True
