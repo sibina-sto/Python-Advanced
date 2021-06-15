f0aec608ade32b34053f8f12dc4d80771ed0c621
piziamko <stoyanovasibina@gmail.com>
2021-06-15 20:37:38 +0300

    Update Player.py

--- 09. Workshop/Tic Tac Toe/Player.py
+++ 09. Workshop/Tic Tac Toe/Player.py
@@ -3,5 +3,8 @@ class Player:
         self.name = name
         self.mark = mark
 
+    def __str__(self):
+        return f'Name: {self.name}, Mark: {self.mark}'
+
     def __repr__(self):
-        return f"Name: {self.name} - Mark: {self.mark}"
+        return f'Name: {self.name}, Mark: {self.mark}'
