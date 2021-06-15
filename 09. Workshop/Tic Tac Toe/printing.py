e9c0ae6517924d36fc613f3c2f40ca84ca5a75f3
piziamko <stoyanovasibina@gmail.com>
2021-06-15 20:40:36 +0300

    Update printing.py

--- 09. Workshop/Tic Tac Toe/printing.py
+++ 09. Workshop/Tic Tac Toe/printing.py
@@ -3,16 +3,16 @@ def print_board(board):
         values = []
         for cell in row:
             if cell is None:
-                values.append(" ")
+                values.append(' ')
             else:
                 values.append(cell)
-        values_str = " | ".join(values)
-        print(f"| {values_str} |")
+        values_str = ' | '.join(values)
+        print(f'| {values_str} |')
 
 
 def print_welcome(player):
-    print("This is the numeration of the board:")
-    print("| 1 | 2 | 3 |\n"
-          "| 4 | 5 | 6 |\n"
-          "| 7 | 8 | 9 |\n")
-    print(f"{player.name} starts first!")
+    print('This is the numeration of the board:')
+    print('| 1 | 2 | 3 |')
+    print('| 4 | 5 | 6 |')
+    print('| 7 | 8 | 9 |')
+    print(f'{player.name} starts first!')
